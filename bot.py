eed8aead45219b479fc18a328692608b9460c61b
honeypy <vitaly.esipov@gmail.com>
2017-03-09 15:54:31 +0300

    requirements added

--- bot.py
+++ bot.py
@@ -119,12 +119,15 @@ def main():
     dp.add_error_handler(error)
  
     # Start the Bot
-    updater.start_polling()
+    #updater.start_polling()
  
     # Run the bot until the you presses Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
+    #updater.idle()
+    updater.start_webhook(listen='127.0.0.1', port=5000, url_path=TOKEN)
+    updater.bot.setWebhook(webhook_url='https://118.226.135.41/'+TOKEN,
+                       certificate=open('cert.pem', 'rb'))
  
  
 if __name__ == '__main__':
